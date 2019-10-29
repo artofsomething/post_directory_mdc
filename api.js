48598ffe254103884facd33e3ab2f1ff6e6adb91
reyhermanses <56987672+reyhermanses@users.noreply.github.com>
2019-10-29 15:13:04 +0700

    update routes selain user routes

--- api.js
+++ api.js
@@ -3,7 +3,25 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongo = require('mongoose');
 
-var post_dir = require('./routes/post_dir_route');
+var company_profile = require('./routes/company_profile_route');
+var cashier_shift = require('./routes/cashier_shift_route');
+var detail_po = require('./routes/detail_po_route');
+var detail_sales = require('./routes/detail_sales_route');
+var detail_stock = require('./routes/detail_stock_opname_route');
+var detail_utpt = require('./routes/detail_utpt_route');
+var kategori = require('./routes/kategori_route');
+var log_act = require('./routes/log_activity_route');
+var log_sess = require('./routes/login_session_route');
+var ops_cost = require('./routes/operational_cost_route');
+var products = require('./routes/products_route');
+var purchase_order = require('./routes/purchase_order_route');
+var sales_trans = require('./routes/sales_transaction_route');
+var satuan = require('./routes/satuan_route');
+var stock_move = require('./routes/stock_movement_route');
+var stock_opname = require('./routes/stock_opname_route');
+var supplier = require('./routes/supplier_route');
+var temp_report_route = require('./routes/temp_stock_report_route');
+var utang_piutang = require('./routes/utang_piutang_route');
 var users = require('./routes/user_route')
 
 var db = mongo.connect('mongodb://localhost:27017/PostDirectory', function (err, response) {
@@ -27,7 +45,25 @@ app.use(function (req, res, next) {
 });
 
 
-app.use('/postdir', post_dir);
+app.use('/companyProfile', company_profile);
+app.use('/cashierShift', cashier_shift);
+app.use('/detailPo', detail_po);
+app.use('/detailSales', detail_sales);
+app.use('/detailStock', detail_stock);
+app.use('/detailUptp', detail_utpt);
+app.use('/kategori', kategori);
+app.use('/logActivity', log_act);
+app.use('/logSession', log_sess);
+app.use('/opsCost', ops_cost);
+app.use('/products', products);
+app.use('/purchaseOrder', purchase_order);
+app.use('/salesTransaction', sales_trans);
+app.use('/satuan', satuan);
+app.use('/stockMove', stock_move);
+app.use('/stockOpname', stock_opname);
+app.use('/supplier', supplier);
+app.use('/tempReport', temp_report_route);
+app.use('/utangPiutang', utang_piutang);
 app.use('/user', users);
 
 //var Schema = mongo.Schema;
